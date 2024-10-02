c2c60d3555e74977e2eb154f783bd72077614043
marcoLapiello <marcolapiello@gmail.com>
2024-10-02 08:41:19 +0200

    Better responsivness

--- templates.js
+++ templates.js
@@ -30,6 +30,7 @@ function getOverlayTemplate(responseAsJson, index, nextIndex, previousIndex) {
   const spriteUrlSvg = `${spritesBaseUrl}${index}.svg`;
 
   return /*html*/ `
+    <div class="overlayWrapper">
       <div id="overlayLeft">
         <div id="overlayLeftTop" class="${responseAsJson.types[0].type.name}Bg">
           <img src="${spriteUrlSvg}" alt="" />
@@ -37,34 +38,65 @@ function getOverlayTemplate(responseAsJson, index, nextIndex, previousIndex) {
   
         <div id="overlayLeftBottom">
           <h3>#${responseAsJson.id} ${responseAsJson.name.charAt(0).toUpperCase() + responseAsJson.name.slice(1)}</h3>
-          <p>Height</p><p>${responseAsJson.height}</p>
-          <p>Weight</p><p>${responseAsJson.weight}</p>
-          <p>Element</p><p id="overlayElement"></p>
+          <div class="overlayLineWrapper">
+            <p>Height</p><p>${responseAsJson.height * 10} Cm</p>
+          </div>
           
+          <div class="overlayLineWrapper">
+            <p>Weight</p><p>${responseAsJson.weight / 10} Kg</p>
+          </div>
+          
+          <h3 id="overlayElementH3">Element</h3>
+          
+          <p id="overlayElement"></p> 
         </div>
       </div>
   
       <div id="overlayRight">
         <div id="overlayRightTop">
           <h3>Stats</h3>
-          <p>HP</p><p>${responseAsJson.stats[0].base_stat}</p>
-          <p>Attack</p><p>${responseAsJson.stats[1].base_stat}</p>
-          <p>Defense</p><p>${responseAsJson.stats[2].base_stat}</p>
-          <p>Special Attack</p><p>${responseAsJson.stats[3].base_stat}</p>
-          <p>Special Defense</p><p>${responseAsJson.stats[4].base_stat}</p>
-          <p>Speed</p><p>${responseAsJson.stats[5].base_stat}</p>
+
+          <div class="overlayLineWrapper">
+            <p>HP</p><p>${responseAsJson.stats[0].base_stat}</p>
+          </div>
+          
+          <div class="overlayLineWrapper">
+            <p>Attack</p><p>${responseAsJson.stats[1].base_stat}</p>
+          </div>
+          
+          <div class="overlayLineWrapper">
+            <p>Defense</p><p>${responseAsJson.stats[2].base_stat}</p>
+          </div>
+          
+          <div class="overlayLineWrapper">
+            <p>Special Attack</p><p>${responseAsJson.stats[3].base_stat}</p>
+          </div>
+          
+          <div class="overlayLineWrapper">
+            <p>Special Defense</p><p>${responseAsJson.stats[4].base_stat}</p>
+          </div>
+          
+          <div class="overlayLineWrapper">
+            <p>Speed</p><p>${responseAsJson.stats[5].base_stat}</p>
+          </div>
+          
         </div>
   
         <div id="overlayRightBottom">
           <h3>Abilities</h3>
           
         </div>
-
-        <button onclick="closeOverlay(${index})">X</button>
-        <button id="nextBtn" onclick="nextPokemon(${nextIndex})">></button>
-        <button id="prevBtn" onclick="previousPokemon(${previousIndex})"><</button>
       </div>
-    `;
+    </div>
+
+    <div class="overlayBtnDiv">
+      <button id="prevBtn" onclick="previousPokemon(${previousIndex})"><</button>
+      <button id="closeBtn" onclick="closeOverlay(${index})">X</button>
+      <button id="nextBtn" onclick="nextPokemon(${nextIndex})">></button> 
+    </div>
+    
+      
+  `;
 }
 
 function getAbilityTemplate(Ability) {
